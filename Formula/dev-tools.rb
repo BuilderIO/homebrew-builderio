9b82e2db49bcbb42054e842fd6f9b9e865dd2a5c
GitHub Action <action@github.com>
2026-09-23 18:06:29 +0000

    Auto-update dev-tools formula

--- Formula/dev-tools.rb
+++ Formula/dev-tools.rb
@@ -3,8 +3,8 @@ require "language/node"
 class DevTools < Formula
   desc "Builder.io AI Powered Design to Code"
   homepage "https://www.builder.io/"
-  url "https://registry.npmjs.org/@builder.io/dev-tools/-/dev-tools-1.86.1.tgz"
-  sha256 "9cfe56fd82682fc995ce76f43e4de23b331c58b7001c59af758a1614ae1f16f8"
+  url "https://registry.npmjs.org/@builder.io/dev-tools/-/dev-tools-1.86.4.tgz"
+  sha256 "05d889eee9e7fe918fc55baa3f8233a77cf6f72f5d17f6954553d5f016ca6226"
 
   license "MIT"
 
